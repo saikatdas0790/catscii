0e374e7594de11bf5e60cc9c3fc0cdea5943851e
Saikat Das <saikatdas0790@gmail.com>
2025-03-18 16:14:28 +0000

    add image processing and ASCII art generation for cat images

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use pretty_hex::PrettyHex;
+use image::load_from_memory;
 
 #[tokio::main]
 async fn main() {
-    let image_bytes = get_cat_image_bytes().await.unwrap();
-    println!("{:?}", &image_bytes[..200].hex_dump());
+    let art = get_cat_ascii_art().await.unwrap();
+    println!("{art}");
 }
 
-async fn get_cat_image_bytes() -> color_eyre::Result<Vec<u8>> {
+async fn get_cat_ascii_art() -> color_eyre::Result<String> {
     use serde::Deserialize;
 
     #[derive(Deserialize)]
@@ -26,12 +26,17 @@ async fn get_cat_image_bytes() -> color_eyre::Result<Vec<u8>> {
         .pop()
         .ok_or_else(|| color_eyre::eyre::eyre!("The cat API returned no images"))?;
 
-    Ok(client
+    let image_bytes = client
         .get(image.url)
         .send()
         .await?
         .error_for_status()?
         .bytes()
         .await?
-        .to_vec())
+        .to_vec();
+
+    let image = load_from_memory(&image_bytes)?;
+    let ascii_art = artem::convert(image, artem::options::OptionBuilder::new().build());
+
+    Ok(ascii_art)
 }
