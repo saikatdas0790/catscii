3899ed9a564168dd14a53241ccce5517ed7d008e
Saikat Das <saikatdas0790@gmail.com>
2025-03-18 14:44:39 +0000

    initialize catscii project with basic API call to The Cat API

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#[tokio::main]
+async fn main() {
+    let res = reqwest::get("https://api.thecatapi.com/v1/images/search")
+        .await
+        .unwrap();
+
+    if !res.status().is_success() {
+        panic!("Request failed with status: {}", res.status());
+    }
+
+    use serde::Deserialize;
+
+    #[derive(Deserialize)]
+    struct CatImage {
+        url: String,
+    }
+
+    let images: Vec<CatImage> = res.json().await.unwrap();
+
+    let image = images
+        .first()
+        .expect("the cat API should return at least one image");
+    println!("The image is at {}", image.url);
+}
